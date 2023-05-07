c725c7f29deac6bade4594d52791a33e44f88d96
Umberto Leonetti <betoneto201@gmail.com>
2023-05-07 15:49:40 -0300

    tela de cadastro

--- TrabalhosWEB1/Trabalho1/scriptLogin.js
+++ TrabalhosWEB1/Trabalho1/scriptLogin.js
@@ -2,7 +2,7 @@ function autenticar() {
 	let loginInput = document.getElementById('login').value.trim();
 	let passwordInput = document.getElementById('password').value.trim();
 	if (loginInput == "" || passwordInput == "") {
-		alert("“Usuário não autenticado");
+		alert("Informe os dados de autenticação");
 	} else {
 		localStorage.setItem('authenticatedUser', loginValue);
 		window.location.href = 'index.html';
