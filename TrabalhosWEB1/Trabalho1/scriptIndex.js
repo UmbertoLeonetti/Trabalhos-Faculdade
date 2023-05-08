4a0f9e044a44139936ba21731571682625ef0a2f
AugustoJDalpra <aarraga@furb.br>
2023-05-07 21:01:20 -0300

    pagina inicial

---
+++ TrabalhosWEB1/Trabalho1/scriptIndex.js
@@ -0,0 +1,11 @@
+function onInit() {
+var login = "Não autenticado";
+
+localStorage.clear();
+if(localStorage.getItem("login") != null){
+    login = localStorage.getItem("login").trim();
+}
+
+document.getElementById("login").innerHTML = login;
+console.log("lOGIN: "+login);
+}
